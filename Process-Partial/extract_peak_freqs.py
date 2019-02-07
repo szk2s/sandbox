1910244d8da8ff366932351553bf4bbb7576112d
szk2s <suzukisjss@gmail.com>
2019-02-07 22:46:44 +0900

    Complete extraction
    - json export and reconstructed wav file export

--- Process-Partial/extract_peak_freqs.py
+++ Process-Partial/extract_peak_freqs.py
@@ -2,105 +2,148 @@
 # coding: utf-8
 
 # In[1]:
+import sys
+import os
 
+sys.path.append('./src')
 import sklearn.cluster as cl
 import numpy as np
 import json
-import matplotlib.pyplot as plt
 import plotly.offline as py
 import plotly.graph_objs as go
-from scipy.signal import hilbert
+import matplotlib.pyplot as plt
+import convert
+from scipy.signal import savgol_filter
+import sounddevice as sd
+import soundfile as sf
 
 py.init_notebook_mode(connected=True)
 
 # In[2]:
 
+input_filename = 'church2.json'
+file = open('./assets/json/'+input_filename, 'r')
+json_obj = json.load(file)
 
-file = open("./assets/json/church2.json", 'r')
-matlab_obj = json.load(file)
-soundname = matlab_obj.get('soundname')
-print('soundname:', soundname)
-
-# In[3]:
-
-
-# # Mock Input
-# mat_times = np.array([0, 0.3, 0.6, 0.9])
-# mat_freqs = np.array([440, 880])
-# mat_amps = np.array([[1,2,3,4],[5,6,7,8]])
+# %%
+points = np.array(json_obj['partials'])
 
 
-# In[4]:
+# In[12]: Plot Scatter
+
+def setup_fig():
+    trace = go.Scatter3d(
+        x=points[:, 0],
+        y=points[:, 1],
+        z=points[:, 2],
+        mode='markers',
+        marker=dict(
+            size=2,
+            opacity=0.8,
+            color=np.log(points[:, 2] * 255)
+        )
+    )
+
+    data = [trace]
+
+    layout = go.Layout(
+        title='Partial Data',
+        hovermode='closest',
+        height=900,
+        margin=dict(
+            l=0,
+            r=0,
+            b=0,
+            t=0
+        ),
+        scene=dict(
+            xaxis=dict(
+                title='Time',
+                ticklen=5,
+                gridwidth=2,
+            ),
+            yaxis=dict(
+                title='Frequency',
+                type='log',
+                ticklen=5,
+                gridwidth=2,
+            ),
+            zaxis=dict(
+                title='Amplitude',
+                ticklen=5,
+                gridwidth=2,
+            )
+        )
+    )
+    return go.Figure(data, layout)
 
 
-# Input
-mat_times = np.array(matlab_obj.get('times'))
-mat_freqs = np.array(matlab_obj.get('freqs'))
-mat_amps = np.array(matlab_obj.get('amps'))
+py.plot(setup_fig(), filename='./plotly/partials.html')
 
-# In[5]:
+# In[13]: Plot Surface
+times, freqs, amps = convert.to_matrix(points)
 
 
-# Make 2d Matrix
+def setup_fig():
+    data = [
+        go.Surface(
+            x=times,
+            y=freqs,
+            z=amps
+        )
+    ]
+    layout = go.Layout(
+        height=900,
+        margin=dict(
+            l=0,
+            r=0,
+            b=0,
+            t=0
+        ),
+        scene=dict(
+            xaxis=dict(
+                title='Time',
+                ticklen=5,
+                gridwidth=2,
+            ),
+            yaxis=dict(
+                title='Frequency',
+                type='log',
+                ticklen=5,
+                gridwidth=2,
+            ),
+            zaxis=dict(
+                title='Amplitude',
+                ticklen=5,
+                gridwidth=2,
+            )
+        )
+    )
+    return go.Figure(data=data, layout=layout)
 
-amps_2d = mat_amps
 
-n_times = mat_amps.shape[1]
-n_freqs = mat_amps.shape[0]
-
-times_2d = np.empty(shape=(1, n_times))
-times_2d[0, :] = mat_times
-times_2d = np.repeat(times_2d, n_freqs, axis=0)
-
-freqs_2d = np.empty(shape=(n_freqs, 1))
-freqs_2d[:, 0] = mat_freqs
-freqs_2d = np.repeat(freqs_2d, n_times, axis=1)
-
-# tmp = np.array(matlab_obj.get('freqs'))
-# mat_freqs = mat_freqs.T
-# mat_times = np.array(matlab_obj.get('times'))
-
-
-# In[6]:
-
-
-x = times_2d.flatten()
-y = freqs_2d.flatten()
-z = amps_2d.flatten()
-
-points = np.column_stack([x, y, z]);
-
+py.plot(setup_fig(), filename='./plotly/midi_amplitude_surface.html')
 # In[7]:
 
-
-isSignal = z > 0.04
+isSignal = points[:, 2] > 0.006
 isSignal
 points = points[isSignal]
-x = points[:, 0]
-y = points[:, 1]
-z = points[:, 2]
 
 # In[8]:
 
 
-# Clustering
-n_clusters = 4
+# DBSCAN Clustering
+x = points[:, 0]
+y = points[:, 1]
+z = points[:, 2]
 
-# specimens = points
-# specimens = np.column_stack([x, np.log2(y), np.log2(z)])
 spX = x / x.max()
 spY = np.log2(y) / np.log2(y).max()
 spZ = z / z.max()
-# specimens = np.column_stack([spX*10, spY*10, spZ*10])
+
 specimens = np.column_stack([spX * 10, spY * 10])
-print(specimens)
-print([spX.min(), spY.min(), spZ.min()])
-print([spX.max(), spY.max(), spZ.max()])
 
-# clustering = cl.AgglomerativeClustering(n_clusters).fit(specimens)
-# print(clustering) 
-
-clustering = cl.DBSCAN(min_samples=7).fit(specimens)
+clustering = cl.DBSCAN(min_samples=50).fit(specimens)
 labels = clustering.labels_
 
 print('<< labels >>')
@@ -108,54 +151,298 @@ print(labels)
 print('length:', len(labels))
 print('types:', np.max(labels) + 1)
 
+
 # In[9]:
 
 
 ## Plot
 
-trace = go.Scatter3d(
-    x=points[:, 0],
-    y=points[:, 1],
-    z=points[:, 2],
-    mode='markers',
-    marker=dict(
-        size=2,
-        opacity=0.8,
-        color=labels
-    )
-)
-
-data = [trace]
-
-layout = go.Layout(
-    title='Partial Data',
-    hovermode='closest',
-    height=800,
-    margin=dict(
-        l=0,
-        r=0,
-        b=0,
-        t=0
-    ),
-    scene=dict(
-        xaxis=dict(
-            title='Time',
-            ticklen=5,
-            gridwidth=2,
-        ),
-        yaxis=dict(
-            title='Frequency',
-            type='log',
-            ticklen=5,
-            gridwidth=2,
-        ),
-        zaxis=dict(
-            title='Amplitude',
-            ticklen=5,
-            gridwidth=2,
+def setup_fig():
+    trace = go.Scatter3d(
+        x=points[:, 0],
+        y=points[:, 1],
+        z=points[:, 2],
+        mode='markers',
+        marker=dict(
+            size=2,
+            opacity=0.8,
+            color=labels
         )
     )
-)
-fig = go.Figure(data, layout)
-py.plot(fig, filename='./plotly/extracted.html')
 
+    data = [trace]
+
+    layout = go.Layout(
+        title='Partial Data',
+        hovermode='closest',
+        height=800,
+        margin=dict(
+            l=0,
+            r=0,
+            b=0,
+            t=0
+        ),
+        scene=dict(
+            xaxis=dict(
+                title='Time',
+                ticklen=5,
+                gridwidth=2,
+            ),
+            yaxis=dict(
+                title='Frequency',
+                type='log',
+                ticklen=5,
+                gridwidth=2,
+            ),
+            zaxis=dict(
+                title='Amplitude',
+                ticklen=5,
+                gridwidth=2,
+            )
+        )
+    )
+    return go.Figure(data, layout)
+
+
+py.plot(setup_fig(), filename='./plotly/extracted.html')
+
+# %%
+points = points[labels > -1, :]
+
+# %%
+n_clusters = 6
+
+x = points[:, 0]
+y = points[:, 1]
+z = points[:, 2]
+
+spX = x / x.max()
+spY = np.log2(y) / np.log2(y).max()
+spZ = z / z.max()
+
+specimens = np.column_stack([spX * 5, spY * 100, spZ * 10])
+
+clustering = cl.AgglomerativeClustering(n_clusters).fit(specimens)
+labels = clustering.labels_
+
+print('<< labels >>')
+print(labels)
+print('length:', len(labels))
+print('types:', np.max(labels) + 1)
+
+
+# In[9]:
+
+## Plot
+
+def setup_fig():
+    trace = go.Scatter3d(
+        x=points[:, 0],
+        y=points[:, 1],
+        z=points[:, 2],
+        mode='markers',
+        marker=dict(
+            size=2,
+            opacity=0.8,
+            color=labels
+        )
+    )
+
+    data = [trace]
+
+    layout = go.Layout(
+        title='Partial Data',
+        hovermode='closest',
+        height=800,
+        margin=dict(
+            l=0,
+            r=0,
+            b=0,
+            t=0
+        ),
+        scene=dict(
+            xaxis=dict(
+                title='Time',
+                ticklen=5,
+                gridwidth=2,
+            ),
+            yaxis=dict(
+                title='Frequency',
+                type='log',
+                ticklen=5,
+                gridwidth=2,
+            ),
+            zaxis=dict(
+                title='Amplitude',
+                ticklen=5,
+                gridwidth=2,
+            )
+        )
+    )
+    return go.Figure(data, layout)
+
+
+py.plot(setup_fig(), filename='./plotly/extracted.html')
+
+# %%
+target_label = 2
+target_cluster = points[labels == target_label, :]
+
+
+## Plot
+
+def setup_fig():
+    trace = go.Scatter3d(
+        x=target_cluster[:, 0],
+        y=target_cluster[:, 1],
+        z=target_cluster[:, 2],
+        mode='markers',
+        marker=dict(
+            size=2,
+            opacity=0.8,
+        )
+    )
+
+    data = [trace]
+
+    layout = go.Layout(
+        title='Partial Data',
+        hovermode='closest',
+        height=800,
+        margin=dict(
+            l=0,
+            r=0,
+            b=0,
+            t=0
+        ),
+        scene=dict(
+            xaxis=dict(
+                title='Time',
+                ticklen=5,
+                gridwidth=2,
+            ),
+            yaxis=dict(
+                title='Frequency',
+                type='log',
+                ticklen=5,
+                gridwidth=2,
+            ),
+            zaxis=dict(
+                title='Amplitude',
+                ticklen=5,
+                gridwidth=2,
+            )
+        )
+    )
+    return go.Figure(data, layout)
+
+
+py.plot(setup_fig(), filename='./plotly/extracted.html')
+
+# %% extract loudest partial in the cluster
+extracted_points = np.empty(shape=(0, 3))
+
+for target_label in range(n_clusters):
+    # print('target:', target_label)
+    target_cluster = points[labels == target_label, :]
+    freqs_list = np.unique(target_cluster[:, 1])
+    mean_amps = np.empty(shape=freqs_list.size)
+    for i, freq in enumerate(freqs_list):
+        # print(i, freq)
+        target_partial = target_cluster[target_cluster[:, 1] == freq]
+        mean_amps[i] = np.mean(target_partial[:, 2])
+    loudest_freq = freqs_list[np.where(mean_amps == mean_amps.max())][0]
+    # print('loudest:', loudest_freq)
+    original_points = np.array(json_obj['partials'])
+    points_to_add = original_points[original_points[:, 1] == loudest_freq]
+    extracted_points = np.append(extracted_points, points_to_add, axis=0)
+
+
+# %% Plot
+
+def setup_fig():
+    trace = go.Scatter3d(
+        x=extracted_points[:, 0],
+        y=extracted_points[:, 1],
+        z=extracted_points[:, 2],
+        mode='markers',
+        marker=dict(
+            size=2,
+            opacity=0.8,
+        )
+    )
+
+    data = [trace]
+
+    layout = go.Layout(
+        title='Partial Data',
+        hovermode='closest',
+        height=800,
+        margin=dict(
+            l=0,
+            r=0,
+            b=0,
+            t=0
+        ),
+        scene=dict(
+            xaxis=dict(
+                title='Time',
+                ticklen=5,
+                gridwidth=2,
+            ),
+            yaxis=dict(
+                title='Frequency',
+                type='log',
+                ticklen=5,
+                gridwidth=2,
+            ),
+            zaxis=dict(
+                title='Amplitude',
+                ticklen=5,
+                gridwidth=2,
+            )
+        )
+    )
+    return go.Figure(data, layout)
+
+py.plot(setup_fig(), filename='./plotly/extracted.html')
+
+# %% json write
+json_obj['extracted_partials'] = extracted_points.tolist()
+with open('./output/json/church2.json', 'w') as outfile:
+    json.dump(json_obj, outfile)
+
+# %% make reconstructed wav file
+sr = 48000
+
+freqs = np.unique(extracted_points[:, 1])
+sines = np.zeros(shape=(np.ceil(extracted_points[:, 0].max() * sr).astype(int), freqs.size))
+for i, freq in enumerate(freqs):
+    partial = extracted_points[extracted_points[:, 1] == freq]
+    partial = partial[partial[:, 0].argsort()]
+    endtime = partial[:, 0].max()
+    times = np.arange(0, endtime, 1 / sr)
+    amps = np.interp(times, partial[:, 0], partial[:, 2])
+    amps = savgol_filter(amps, sr//10+1, 3)
+    x = times
+    y = amps * np.sin(2 * np.pi * freq * x)
+    sines[:, i] = y
+
+reconstructed = np.sum(sines, axis=1)
+reconstructed = reconstructed/abs(reconstructed).max()
+
+# %%
+for column in sines.T:
+    plt.plot(column)
+plt.show()
+
+
+# %%
+
+plt.plot(sines[:, 4])
+plt.show()
+
+# %% play and export
+sd.play(reconstructed, sr)
+output_filename = os.path.splitext(input_filename)[0] + '.wav'
+sf.write('./output/wav/reconstructed/' + output_filename, reconstructed, sr)
