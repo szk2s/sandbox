8765b76ffd5dfbfe0f2d67aad250efb7cc01cfe9
szk2s <suzukisjss@gmail.com>
2019-02-07 22:47:35 +0900

    Remove module dependency

--- Process-Partial/wavelet_transform.m
+++ Process-Partial/wavelet_transform.m
@@ -1,8 +1,7 @@
-
 %% Audio import
 input_filepath = './assets/wav/church2.wav';
 [audio, Fs] = audioread(input_filepath);
-[audio, Fs] = prepareAnalysis(audio, Fs);  %sum to mono and normalize audio 
+[audio, Fs] = prepareAnalysis(audio, Fs);  %sum to mono and normalize audio
 
 % audio = audio(0.5*Fs:1.5*Fs); %if you want to crop input file.
 endtime = length(audio) / Fs;
@@ -11,7 +10,7 @@ sound(audio, Fs);
 
 [cfs,f] = cwt(audio',Fs,'WaveletParameters',[14,200]);
 p = abs(cfs).^2;
-p = normalizeMatrix(p); 
+p = p / max(p(:));
 p = p.*0.999;%  normalize
 sampleIndices = 0:1:size(audio,1)-1;
 t = sampleIndices / Fs;
@@ -27,3 +26,17 @@ clear sampleIndices
 
 save(strcat('./mat/', name, '.mat'))
 
+%% define sub function
+
+function [result, Fs] = prepareAnalysis(audioData, sr)
+    monoAudio = mean(audioData,2);
+    ratio = 1/max(abs(monoAudio));
+    monoAudio = monoAudio * ratio;
+    if sr == 96000
+        result = resample(monoAudio,1,2);
+        Fs = sr/2;
+    else
+        result = monoAudio;
+        Fs = sr;
+    end
+end
