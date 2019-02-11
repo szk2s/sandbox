d11afbff2b2c10a6690b8433c4329a6e0441766a
szk2s <suzukisjss@gmail.com>
2019-02-12 00:40:43 +0900

    update

--- Process-Partial/extract_peak_freqs.py
+++ Process-Partial/extract_peak_freqs.py
@@ -18,29 +18,10 @@ import sounddevice as sd
 import soundfile as sf
 import additive_synth
 import scipy.io as sio
+from rescale import rescale
 
 np.set_printoptions(threshold=9999)
 
-# %%
-def rescale(array, range = (0, 1)):
-    _array = np.copy(array)
-    if range[1] - range[0] < 0:
-        print('Max is smaller than min')
-        print('Parameter "range" should be a tuple like (min, max)')
-        return
-    target_center = sum(range)/2
-    target_delta = range[1] - target_center
-
-    # shift center to 0
-    current_center = (_array.max() + _array.min()) /2
-    _array -= current_center
-    # normalize (range will be (0, 1))
-    _array /= np.max(_array)
-    # multiple to target range
-    _array *= target_delta
-    # shift center
-    _array += target_center
-    return _array
 
 # In[2]:
 
@@ -121,14 +102,14 @@ print('types:', np.max(labels) + 1)
 
 def setup_fig():
     trace = go.Scatter3d(
-        x=points[::10, 0],
-        y=points[::10, 1],
-        z=points[::10, 2],
+        x=points[::100, 0],
+        y=points[::100, 1],
+        z=points[::100, 2],
         mode='markers',
         marker=dict(
             size=2,
             opacity=0.8,
-            color=labels[::10]
+            # color=labels[::10]
         )
     )
 
