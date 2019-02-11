8cceb7e7b4f1ed56e3a85e99c304dbc7c35e53ce
szk2s <suzukisjss@gmail.com>
2019-02-12 02:38:26 +0900

    Complete

--- Process-Partial/extract_peak_freqs.py
+++ Process-Partial/extract_peak_freqs.py
@@ -18,30 +18,27 @@ import sounddevice as sd
 import soundfile as sf
 import additive_synth
 import scipy.io as sio
+from rescale import rescale
 
 np.set_printoptions(threshold=9999)
 
 
-# %%
-def rescale(x, range=(-1, 1), axis=None):
-    domain = np.min(x, axis), np.max(x, axis)
-    y = (x - (domain[1] + domain[0]) / 2) / (domain[1] - domain[0])
-    return y * (range[1] - range[0]) + (range[1] + range[0]) / 2
-
-
 # In[2]:
 
-input_filename = 'cricket_drone_1.json'
+input_filename = 'frogs_short.json'
 file = open('./assets/json/' + input_filename, 'r')
 json_obj = json.load(file)
 
 # %%
 points = np.array(json_obj['partials'])
+points[:, 2] = np.log(points[:, 2])
+points[:, 2] = rescale(points[:, 2])
+
 
 # In[7]:
-threshold = -36  # in dB
+# threshold = -36  # in dB
 
-isSignal = points[:, 2] > 10 ** (threshold / 20)
+isSignal = points[:, 2] > 0.8
 isSignal
 points = points[isSignal]
 
@@ -53,11 +50,13 @@ freq_range = dict(
 condition = np.logical_and(points[:, 1] > freq_range['low'], points[:, 1] < freq_range['high'])
 points = points[condition]
 
-# %% DBSCAN Clustering
+
+# %%  MiniBatchKMeans Clustering (fastest method)
+n_clusters = 2
 weight = dict(
     times=0.5,
     freqs=5,
-    amps=0.2,
+    amps=2,
 )
 
 min_samples = 50
@@ -78,7 +77,7 @@ specimens = np.column_stack(
     ]
 )
 
-clustering = cl.DBSCAN(min_samples=min_samples).fit(specimens)
+clustering = cl. MiniBatchKMeans(n_clusters).fit(specimens)
 labels = clustering.labels_
 
 print('<< labels >>')
@@ -86,40 +85,7 @@ print(labels)
 print('length:', len(labels))
 print('types:', np.max(labels) + 1)
 
-# %%
-points = points[labels > -1, :]
 
-# %% Agglomerative Clustering
-n_clusters = 15
-weight = dict(
-    times=0.1,
-    freqs=10,
-    amps=0.5,
-)
-
-x = points[:, 0]
-y = points[:, 1]
-z = points[:, 2]
-
-spX = x / x.max()
-spY = np.log2(y) / np.log2(y).max()
-spZ = z / z.max()
-
-specimens = np.column_stack(
-    [
-        spX * 5 * weight['times'],
-        spY * 100 * weight['freqs'],
-        spZ * 10 * weight['amps'],
-    ]
-)
-
-clustering = cl.AgglomerativeClustering(n_clusters).fit(specimens)
-labels = clustering.labels_
-
-print('<< labels >>')
-print(labels)
-print('length:', len(labels))
-print('label types:', np.max(labels) + 1)
 
 
 # In[9]:
@@ -129,14 +95,14 @@ print('label types:', np.max(labels) + 1)
 
 def setup_fig():
     trace = go.Scatter3d(
-        x=points[:, 0],
-        y=points[:, 1],
-        z=points[:, 2],
+        x=points[::10, 0],
+        y=points[::10, 1],
+        z=points[::10, 2],
         mode='markers',
         marker=dict(
             size=2,
             opacity=0.8,
-            color=labels
+            color=labels[::10]
         )
     )
 
@@ -233,7 +199,7 @@ py.plot(setup_fig(), filename='./plotly/extracted.html')
 
 # %% extract loudest partial in the cluster
 extracted_points = np.empty(shape=(0, 3))
-original_points = np.array(json_obj['partials'])
+original_points = np.copy(points)
 
 for target_label in range(n_clusters):
     # print('target:', target_label)
@@ -302,11 +268,11 @@ def setup_fig():
 py.plot(setup_fig(), filename='./plotly/extracted.html')
 
 # %% Treatment
-condition0 = extracted_points[:, 0] > 3
-condition1 = extracted_points[:, 1] > 6000
-condition2 = extracted_points[:, 2] < 0.015
+# condition0 = extracted_points[:, 0] > 3
+# condition1 = extracted_points[:, 1] > 6000
+condition2 = extracted_points[:, 2] < 0.85
 
-zero_padding_condition = np.logical_and(condition0, np.logical_and(condition1, condition2))
+# zero_padding_condition = np.logical_and(condition0, np.logical_and(condition1, condition2))
 
 
 # %% Plot
@@ -316,11 +282,11 @@ def setup_fig():
         x=extracted_points[:, 0],
         y=extracted_points[:, 1],
         z=extracted_points[:, 2],
-        mode='markers',
+        mode='markers+lines',
         marker=dict(
             size=2,
             opacity=0.8,
-            color=zero_padding_condition * 127,
+            color=condition2 * 127,
         )
     )
 
@@ -362,55 +328,17 @@ py.plot(setup_fig(), filename='./plotly/extracted.html')
 
 # %% Zero padding
 
-extracted_points[zero_padding_condition, 2] = 0
+extracted_points[condition2, 2] = 0.85
 # extracted_points[extracted_points[:, 2] < 0.03, 2] = 0
-extracted_points[:, 2] = rescale(extracted_points[:, 2], range=(0, 1))
+extracted_points[:, 2] = rescale(extracted_points[:, 2])
 
+# %%
+if extracted_points[:, 2].min > 0.:
+    extracted_points[:, 2] = rescale(extracted_points[:, 2])
 # %%
 times, freqs, amps = convert.to_matrix(extracted_points)
 
 
-# In[13]: Plot Surface
-
-def setup_fig():
-    data = [
-        go.Surface(
-            x=times,
-            y=freqs,
-            z=amps
-        )
-    ]
-    layout = go.Layout(
-        height=900,
-        margin=dict(
-            l=0,
-            r=0,
-            b=0,
-            t=0
-        ),
-        scene=dict(
-            xaxis=dict(
-                title='Time',
-                ticklen=5,
-                gridwidth=2,
-            ),
-            yaxis=dict(
-                title='Frequency',
-                type='log',
-                ticklen=5,
-                gridwidth=2,
-            ),
-            zaxis=dict(
-                title='Amplitude',
-                ticklen=5,
-                gridwidth=2,
-            )
-        )
-    )
-    return go.Figure(data=data, layout=layout)
-
-
-py.plot(setup_fig(), filename='./plotly/midi_amplitude_surface.html')
 
 # %%
 json_obj['extracted_partials'] = dict(
