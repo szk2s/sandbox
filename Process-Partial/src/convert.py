31e4c76f179f3d174851b3207824ec8c9c47dca8
szk2s <suzukisjss@gmail.com>
2019-02-11 15:52:01 +0900

    Refactor convert.py
    - Fix bag
    - Add to_points

--- Process-Partial/src/convert.py
+++ Process-Partial/src/convert.py
@@ -11,7 +11,34 @@ def to_matrix(points):
     while not it.finished:
         its_time = times[it.multi_index[1]]
         its_freq = freqs[it.multi_index[0]]
-        idx_in_z = np.logical_and(x == its_time, y == its_freq)
-        amps[it.multi_index] = z[idx_in_z][0]
+        row_idx_in_points = np.logical_and(x == its_time, y == its_freq)
+        if np.sum(row_idx_in_points) > 0:
+            amp_to_add = z[row_idx_in_points][0]
+        if np.sum(row_idx_in_points) == 0:
+            amp_to_add = 0
+        amps[it.multi_index] = amp_to_add
         it.iternext()
     return times, freqs, amps
+
+
+def to_points(times, freqs, amps):
+    amps_2d = amps
+
+    n_times = amps_2d.shape[1]
+    n_freqs = amps_2d.shape[0]
+
+    times_2d = np.empty(shape=(1, n_times))
+    times_2d[0, :] = times
+    times_2d = np.repeat(times_2d, n_freqs, axis=0)
+
+    freqs_2d = np.empty(shape=(n_freqs, 1))
+    freqs_2d[:, 0] = freqs
+    freqs_2d = np.repeat(freqs_2d, n_times, axis=1)
+
+    x = times_2d.flatten()
+    y = freqs_2d.flatten()
+    z = amps_2d.flatten()
+
+    points = np.column_stack([x, y, z])
+
+    return points
